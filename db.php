9f34acae86fcc79397fe33bca2b41b9a3df507f1
Mohamad Akhirudin <31067204+akhirudin08@users.noreply.github.com>
2025-12-21 19:43:52 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
 $host = 'localhost';
-$user = 'pefsdzhc_akhirudin';
-$pass = 'Udin2015'; // Kosongkan jika pakai XAMPP default
-$db   = 'pefsdzhc_portfolio';
+$user = 'root';
+$pass = ''; // Kosongkan jika pakai XAMPP default
+$db   = 'database_portfolio';
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
 ?>
